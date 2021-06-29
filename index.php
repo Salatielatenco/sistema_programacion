7595832b6c83e913118c1958ef5c419654630aa5
salatielatenco <carlossalatielatenco@gmail.com>
2021-06-28 23:48:17 -0500

    actualizacion
    actualizacion

--- index.php
+++ index.php
@@ -9,71 +9,10 @@
   <title>sistema</title>
 </head>
 
-<body style="background-color: gray;">
-  <div class="container">
-    <div class="row">
-      <div class="col"></div>
-      <div class="col" style="background-color: blue;">
-      <h2>SISTEMA DE ARCHIVOS</h2>
-      </div>
-      <div class="col"></div>
-    </div>
-  </div>
-<div class="container mt-2">
-    
-
-    <div class="row justify-content-around py-4 " style="background-color: red;">
-      <div class="col-sm-4 text-center">
-
-        <div class="alert alert-info" role="alert" id="mostrar_mensaje"></div>
-    
-
-      </div>
-      <div class="col-sm-4">
-        <span class="btn btn-primary" id="btn_visitar_documentos">examinar</span>
-        <span class="btn btn-warning" id="btn_visitar_documentos">subir</span>
-        <span class="btn btn-success" id="btn_visitar_documentos">cancelar</span>
-
-      </div>
-    </div>
-  </div>
-
-  <div class="container mt-5">
-    
-
-    <div class="row justify-content-around py-4 " style="background-color: blue;">
-      <div class="col-sm-4 text-center">
-
-        <div class="alert alert-info" role="alert" id="mostrar_mensaje"></div>
-    
-
-      </div>
-      <div class="col-sm-4">
-        <span class="btn btn-primary" id="btn_visitar_documentos">categoria</span>
-        <span class="btn btn-warning" id="btn_visitar_documentos">abrir archivo</span>
-        <span class="btn btn-success" id="btn_visitar_documentos">examinar</span>
-
-      </div>
-      
-    </div>
-    <div class="row justify-content-around py-4 " style="background-color: blue;">
-      <div class="col-sm-4 text-center">
-
-        <div class="alert alert-info" role="alert" id="mostrar_mensaje"></div>
-    
-
-      </div>
-      <div class="col-sm-4">
-        <span class="btn btn-primary" id="btn_visitar_documentos">categoria</span>
-        <span class="btn btn-warning" id="btn_visitar_documentos">abrir archivo</span>
-        <span class="btn btn-success" id="btn_visitar_documentos">examinar</span>
-
-        </div>
-        
-  
+<body style="background-color: redy;">
 
 
-  </div>
+
   
 
 </body>
